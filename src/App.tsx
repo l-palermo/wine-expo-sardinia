6bb53ebc8c375d6928f6f02ffe23b09e6946f385
Luigi Palermo <l-palermo@hotmail.it>
2022-03-07 17:37:45 +0000

    add mobile break points

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import "./App.css";
 import NavigationBar from "./components/navigation-bar";
 import BackgroundImage from "./components/background-image";
 import PageContent from "./components/page-content";
-import Email from "./components/email";
 import FooterBar from "./components/footer-bar";
 import { ReactElement } from "react";
 
@@ -11,12 +10,9 @@ function App(): ReactElement {
     <div className="app">
       <BackgroundImage />
       <header className="pageWrapper">
-        {/* <Navigation /> */}
         <NavigationBar />
         <PageContent />
-        <Email />
         <FooterBar />
-        {/* <Footer /> */}
       </header>
     </div>
   );
--- src/components/page-content/page-content.tsx
+++ src/components/page-content/page-content.tsx
@@ -1,11 +1,15 @@
+import Email from "../email";
 import "./page-content.css";
 
 function PageContent() {
   return (
-    <div className="pageContent">
-      <p className="text">Coming soon</p>
-      <h2 className="text">20 - 21 - 22</h2>
-      <h2 className="text">Maggio</h2>
+    <div className="pageContentWrapper">
+        <div className="comingSoon">
+          <p className="text">Coming soon</p>
+          <h2 className="text">20 - 21 - 22</h2>
+          <h2 className="text">Maggio 2022</h2>
+        </div>
+        <Email />
     </div>
   );
 }
