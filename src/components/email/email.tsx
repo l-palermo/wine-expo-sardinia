5e651f063a75bf86aae097eb13046b203a4b98ad
Luigi Palermo <l-palermo@hotmail.it>
2022-03-15 21:06:25 +0000

    make email link open to new tab

--- src/components/email/email.tsx
+++ src/components/email/email.tsx
@@ -2,7 +2,12 @@ import "./email.css";
 
 function Email() {
   return (
-    <a className="email" href="mailto:info@winexposardinia.it">
+    <a
+      className="email"
+      href="mailto:info@winexposardinia.it"
+      target="_blank"
+      rel="noreferrer"
+    >
       <h4 className="contattaci">Contattaci</h4>
       <h3 className="emailAddress">info@winexposardinia.it</h3>
     </a>
