4123adc7d5b34b8667ea7f2f0d45c4a6b4f33939
Luigi Palermo <l-palermo@hotmail.it>
2022-03-23 19:22:47 +0000

    change dates to wip

--- src/components/page-content/page-content.tsx
+++ src/components/page-content/page-content.tsx
@@ -5,9 +5,9 @@ function PageContent() {
   return (
     <div className="pageContentWrapper">
         <div className="comingSoon">
-          <p className="text textComingSoon">Coming soon</p>
-          <h2 className="text">20 - 21 - 22</h2>
-          <h2 className="text">Maggio 2022</h2>
+          <p className="text textComingSoon">Work in progress</p>
+          {/* <h2 className="text">20 - 21 - 22</h2>
+          <h2 className="text">Maggio 2022</h2> */}
         </div>
         <Email />
     </div>
